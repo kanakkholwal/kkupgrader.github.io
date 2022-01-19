95f4f4206035eaa58c962536175e3c9ce6a74298
K K UPGRADER <57281597+KKUPGRADER@users.noreply.github.com>
2022-01-19 18:29:27 +0530

    Create Optional-Lazy-Google-Code-Font.js

---
+++ plugins/CodeBlock_Syntax-Highlighter_Copy-Button/Optional-Lazy-Google-Code-Font.js
@@ -0,0 +1,2 @@
+function loadCSS(e, t, n) { "use strict"; var i = window.document.createElement("link"); var o = t || window.document.getElementsByTagName("script")[0]; i.rel = "stylesheet"; i.href = e; i.media = "only x"; o.parentNode.insertBefore(i, o); setTimeout(function () { i.media = n || "all" }) }
+loadCSS("https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,200;0,300;0,400;1,200;1,300;1,400&display=swap");
