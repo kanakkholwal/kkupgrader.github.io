8d413d2d29b54758b08c2220c0c3be3663a639e2
Kanak <kkupgrader.fs@gmail.com>
2022-02-22 10:16:41 +0530

    .

--- main.js
+++ main.js
@@ -73,6 +73,22 @@ if ($("pre").length) {
       document.body.appendChild(copy_btn);
   }
 }
+(function () {
+  var observer = new IntersectionObserver(onIntersect);
+  document.querySelectorAll("[data-src]").forEach((img) => {
+    observer.observe(img);
+  });
+
+  function onIntersect(entries) {
+    entries.forEach((entry) => {
+      if (entry.target.getAttribute("data-processed") || !entry.isIntersecting)
+        return true;
+      entry.target.setAttribute("src", entry.target.getAttribute("data-src"));
+      entry.target.setAttribute("data-processed", true);
+      entry.target.classList.remove("lazy-shimmer");
+    });
+  }
+})();
 
 //var script = document.createElement("script");
 /////script.src = "";
