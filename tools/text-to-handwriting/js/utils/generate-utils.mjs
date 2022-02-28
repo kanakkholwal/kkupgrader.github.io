c5da306295dec400bdbea6170bf13719429d34af
Kanak <kkupgrader.fs@gmail.com>
2022-02-28 13:03:41 +0530

    .

--- tools/text-to-handwriting/js/utils/generate-utils.mjs
+++ tools/text-to-handwriting/js/utils/generate-utils.mjs
@@ -5,87 +5,86 @@ const overlayEl = document.querySelector('.overlay');
 let paperContentPadding;
 
 function isFontErrory() {
-  // SOme fonts have padding top errors, this functions tells you if the current font has that;
-  const currentHandwritingFont = document.body.style.getPropertyValue(
-    '--handwriting-font'
-  );
-  return (
-    currentHandwritingFont === '' ||
-    currentHandwritingFont.includes('Homemade Apple')
-  );
+    // SOme fonts have padding top errors, this functions tells you if the current font has that;
+    const currentHandwritingFont = document.body.style.getPropertyValue(
+        '--handwriting-font'
+    );
+    return (
+        currentHandwritingFont === '' ||
+        currentHandwritingFont.includes('Homemade Apple')
+    );
 }
 
 function applyPaperStyles() {
-  pageEl.style.border = 'none';
-  pageEl.style.overflowY = 'hidden';
+    pageEl.style.border = 'none';
+    pageEl.style.overflowY = 'hidden';
 
-  // Adding class shadows even if effect is scanner
-  if (document.querySelector('#page-effects').value === 'scanner') {
-    overlayEl.classList.add('shadows');
-  } else {
-    overlayEl.classList.add(document.querySelector('#page-effects').value);
-  }
+    // Adding class shadows even if effect is scanner
+    if (document.querySelector('#page-effects').value === 'scanner') {
+        overlayEl.classList.add('shadows');
+    } else {
+        overlayEl.classList.add(document.querySelector('#page-effects').value);
+    }
 
-  if (document.querySelector('#page-effects').value === 'scanner') {
-    // For scanner, we need shadow between 50deg to 120deg only
-    // Since If the lit part happens to be on margins, the margins get invisible
-    overlayEl.style.background = `linear-gradient(${
+    if (document.querySelector('#page-effects').value === 'scanner') {
+        // For scanner, we need shadow between 50deg to 120deg only
+        // Since If the lit part happens to be on margins, the margins get invisible
+        overlayEl.style.background = `linear-gradient(${
       Math.floor(Math.random() * (120 - 50 + 1)) + 50
     }deg, #0008, #0000)`;
-  } else if (document.querySelector('#page-effects').value === 'shadows') {
-    overlayEl.style.background = `linear-gradient(${
+    } else if (document.querySelector('#page-effects').value === 'shadows') {
+        overlayEl.style.background = `linear-gradient(${
       Math.random() * 360
     }deg, #0008, #0000)`;
-  }
+    }
 
-  if (isFontErrory() && document.querySelector('#font-file').files.length < 1) {
-    paperContentPadding =
-      paperContentEl.style.paddingTop.replace(/px/g, '') || 5;
-    const newPadding = Number(paperContentPadding) - 5;
-    paperContentEl.style.paddingTop = `${newPadding}px`;
-  }
+    if (isFontErrory() && document.querySelector('#font-file').files.length < 1) {
+        paperContentPadding =
+            paperContentEl.style.paddingTop.replace(/px/g, '') || 5;
+        const newPadding = Number(paperContentPadding) - 5;
+        paperContentEl.style.paddingTop = `${newPadding}px`;
+    }
 }
 
 function removePaperStyles() {
-  pageEl.style.overflowY = 'auto';
-  pageEl.style.border = '1px solid var(--elevation-background)';
+    pageEl.style.overflowY = 'auto';
+    pageEl.style.border = '1px solid var(--elevation-background)';
 
-  if (document.querySelector('#page-effects').value === 'scanner') {
-    overlayEl.classList.remove('shadows');
-  } else {
-    overlayEl.classList.remove(document.querySelector('#page-effects').value);
-  }
+    if (document.querySelector('#page-effects').value === 'scanner') {
+        overlayEl.classList.remove('shadows');
+    } else {
+        overlayEl.classList.remove(document.querySelector('#page-effects').value);
+    }
 
-  if (isFontErrory()) {
-    paperContentEl.style.paddingTop = `${paperContentPadding}px`;
-  }
+    if (isFontErrory()) {
+        paperContentEl.style.paddingTop = `${paperContentPadding}px`;
+    }
 }
 
 function renderOutput(outputImages) {
-  if (outputImages.length <= 0) {
-    document.querySelector('#output').innerHTML =
-      'Click "Generate Image" Button to generate new image.';
-    document.querySelector('#download-as-pdf-button').classList.remove('show');
-    document.querySelector('#delete-all-button').classList.remove('show');
-    return;
-  }
+    if (outputImages.length <= 0) {
+        document.querySelector('#output').innerHTML =
+            'Click "Generate Image" Button to generate new image.';
+        document.querySelector('#download-as-pdf-button').classList.remove('show');
+        document.querySelector('#delete-all-button').classList.remove('show');
+        return;
+    }
 
-  document.querySelector('#download-as-pdf-button').classList.add('show');
-  document.querySelector('#delete-all-button').classList.add('show');
-  document.querySelector('#output').innerHTML = outputImages
-    .map(
-      (outputImageCanvas, index) => /* html */ `
+    document.querySelector('#download-as-pdf-button').classList.add('show');
+    document.querySelector('#delete-all-button').classList.add('show');
+    document.querySelector('#output').innerHTML = outputImages
+        .map(
+            (outputImageCanvas, index) => /* html */ `
     <div 
       class="output-image-container" 
       style="position: relative;display: inline-block;"
     >
       <button 
         data-index="${index}" 
-        class="close-button close-${index}">
-          &times;
+        class="close-button btn-close p-2 active bg-light shadow-4-strong close-${index}">
       </button>
       <img 
-        class="shadow" 
+        class="shadow-5-strong" 
         alt="Output image ${index}" 
         src="${outputImageCanvas.toDataURL('image/jpeg')}"
       />
@@ -99,22 +98,20 @@ function renderOutput(outputImages) {
         <br/>
 
         <button 
-          class="button move-left"
-          data-index="${index}" 
+        type="button" class="btn btn-primary prev"          data-index="${index}" 
         >
-          Move Left
+        <i class="fas fa-arrow-left me-2"></i>   Move Left
         </button>
         <button 
-          class="button move-right"
-          data-index="${index}" 
+        type="button" class="btn btn-primary next"           data-index="${index}" 
         >
-          Move Right
+          Move Right <i class="fas fa-arrow-right ms-2"></i>
         </button>
       </div>
     </div>
     `
-    )
-    .join('');
+        )
+        .join('');
 }
 
 export { removePaperStyles, applyPaperStyles, renderOutput };
