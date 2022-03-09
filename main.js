ebeebff29953abffc55fe015ad1884498b888db9
Kanak <kkupgrader.fs@gmail.com>
2022-03-09 19:13:56 +0530

    .

--- main.js
+++ main.js
@@ -14,8 +14,9 @@
         });
     }
 })();
+//var creditsyear = new Date().getFullYear();
 
-document.querySelector(".date").innerHTML = creditsyear.getFullYear();
+// document.querySelector(".date").InnerHTML = creditsyear;
 
 
 
