36ba67c2f661da7a7d919965eba19bc371eeb027
Kanak <kkupgrader.fs@gmail.com>
2022-03-22 11:32:28 +0530

    .

--- tools/live-code-editor/app.js
+++ tools/live-code-editor/app.js
@@ -1,42 +1,27 @@
-function update() {
-    $("iframe").contents().find("html").html("<html><head><style type='text/css'>" + $("#csspanel").val() + "</style></head><body>" + $(htmlpanel).val() + "</body></html>");
+//function update() {
+//   $("iframe").contents().find("html").html("<html><head><style type='text/css'>" + $("#csspanel").val() + "</style></head><body>" + $(htmlpanel).val() + "</body></html>");
 
-    // 				document.getElementById("outputpanel").contentWindow.eval($("#javascriptpanel").val());
-}
-$(".toggleButtons").hover(
+// 				document.getElementById("outputpanel").contentWindow.eval($("#javascriptpanel").val());
+//}
 
-    function() {
-        $(this).addClass("highlightedButtons");
-        $(this).css('cursor', 'pointer');
 
-    },
-    function() {
-        $(this).removeClass("highlightedButtons")
-    }
-);
-$(".toggleButtons").click(function() {
-    $(this).toggleClass("active");
-    $(this).removeClass("highlightedButtons");
-    var panelId = "#" + $(this).attr("id") + "panel";
-    $(panelId).toggleClass("hidden");
-    var count_hidden = $(".hidden").length;
-    $(".panel").width(($(window).width() / (4 - count_hidden)) - 15);
+//$("iframe").contents().find("html").html($("#htmlpanel").val());
 
+
+var htmlEditor = CodeMirror.fromTextArea(document.getElementById("html"), {
+    lineNumbers: true,
+    mode: 'htmlmixed',
+    theme: 'material',
 });
 
+var cssEditor = CodeMirror.fromTextArea(document.getElementById("css"), {
+    lineNumbers: true,
+    mode: 'css',
+    theme: 'material',
+});
 
-$(".panel").height($(window).height() - $("#header").height() - 15);
-
-
-
-
-$("iframe").contents().find("html").html($("#htmlpanel").val());
-
-
-
-update();
-$("pre").on('change keyup paste', function() {
-    update();
-    Prism.highlightAll();
-
+var jsEditor = CodeMirror.fromTextArea(document.getElementById('js'), {
+    lineNumbers: true,
+    mode: 'javascript',
+    theme: 'material',
 });
