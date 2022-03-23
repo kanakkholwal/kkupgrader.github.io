582c19fca92203ffc5fbce58acc36211ce301f02
Kanak <kkupgrader.fs@gmail.com>
2022-03-23 12:36:49 +0530

    .. blog

--- blog/blog.js
+++ blog/blog.js
@@ -1,9 +1,12 @@
 const PageTitle = document.title;
-document.getElementById("title").innerText = PageTitle;
+const $title = $('.title');
+$title.text(PageTitle);
 document.getElementById("img-alt").setAttribute("alt", PageTitle);
 document.getElementById("img-alt").setAttribute("title", PageTitle);
 
-document.querySelector('#m-share').addEventListener('click', function() {
+const $share = $(".m-share");
+$share.click(function(e) {
+    e.preventDefault();
     if (typeof navigator.share === 'undefined') {
         log("No share API available!");
     } else {
