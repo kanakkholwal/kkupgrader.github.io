df82d967700b8f5810eed110e51d25c8d8300885
Kanak <kkupgrader.fs@gmail.com>
2022-03-23 22:01:14 +0530

    Blog Page updated

--- blog/blog.js
+++ blog/blog.js
@@ -1,5 +1,21 @@
+var comment = true;
+var review = false;
+var rating = false;
+var Widget = true;
+var tocify = true;
+
+
+
+
+
+
+
+
+
 const PageTitle = document.title;
+
 const $title = $('.title');
+
 $title.text(PageTitle);
 document.getElementById("img-alt").setAttribute("alt", PageTitle);
 document.getElementById("img-alt").setAttribute("title", PageTitle);
@@ -18,7 +34,97 @@ $share.click(function(e) {
     }
 });
 
-var comment = true;
-var review = false;
-var rating = false;
-var Widget = true;
+
+if (tocify === true) {
+    /* Table of Content, Credit: blustemy.io/creating-a-table-of-contents-in-javascript */
+
+    class TableOfContents {
+        constructor({ from, to }) {
+            this.fromElement = from;
+            this.toElement = to;
+            this.headingElements = this.fromElement.querySelectorAll("h1, h2, h3, h4, h5, h6");
+            this.tocElement = document.createElement("div");
+        };
+        getMostImportantHeadingLevel() {
+            let mostImportantHeadingLevel = 6;
+            for (let i = 0; i < this.headingElements.length; i++) {
+                let headingLevel = TableOfContents.getHeadingLevel(this.headingElements[i]);
+                mostImportantHeadingLevel = (headingLevel < mostImportantHeadingLevel) ? headingLevel : mostImportantHeadingLevel;
+            }
+            return mostImportantHeadingLevel;
+        };
+        static generateId(headingElement) { return headingElement.textContent.replace(/\s+/g, "_"); };
+        static getHeadingLevel(headingElement) {
+            switch (headingElement.tagName.toLowerCase()) {
+                case "h1":
+                    return 1;
+                case "h2":
+                    return 2;
+                case "h3":
+                    return 3;
+                case "h4":
+                    return 4;
+                case "h5":
+                    return 5;
+                case "h6":
+                    return 6;
+                default:
+                    return 1;
+            }
+        };
+        generateToc() {
+            let currentLevel = this.getMostImportantHeadingLevel() - 1,
+                currentElement = this.tocElement;
+            for (let i = 0; i < this.headingElements.length; i++) {
+                let headingElement = this.headingElements[i],
+                    headingLevel = TableOfContents.getHeadingLevel(headingElement),
+                    headingLevelDifference = headingLevel - currentLevel,
+                    linkElement = document.createElement("a");
+                if (!headingElement.id) { headingElement.id = TableOfContents.generateId(headingElement); }
+                linkElement.href = `#${headingElement.id}`;
+                linkElement.textContent = headingElement.textContent;
+                if (headingLevelDifference > 0) {
+                    for (let j = 0; j < headingLevelDifference; j++) {
+                        let listElement = document.createElement("ol"),
+                            listItemElement = document.createElement("li");
+                        listElement.appendChild(listItemElement);
+                        currentElement.appendChild(listElement);
+                        currentElement = listItemElement;
+                    }
+                    currentElement.appendChild(linkElement);
+                } else {
+                    for (let j = 0; j < -headingLevelDifference; j++) { currentElement = currentElement.parentNode.parentNode; }
+                    let listItemElement = document.createElement("li");
+                    listItemElement.appendChild(linkElement);
+                    currentElement.parentNode.appendChild(listItemElement);
+                    currentElement = listItemElement;
+                }
+                currentLevel = headingLevel;
+            }
+            this.toElement.appendChild(this.tocElement.firstChild);
+        }
+    }
+    /* Activation */
+    document.addEventListener('DOMContentLoaded', () => new TableOfContents({ from: document.querySelector('.post-body'), to: document.querySelector('#tocify') }).generateToc());
+
+    /* Prepend as ShortCode  */
+
+
+    const $menu = $(".toc-bg");
+
+    $(document).ready(function() {
+
+        $('.toc-toggler').click(function() {
+            $menu.toggleClass("show");
+
+        });
+        $(document).mouseup((e) => {
+            $menu.is(e.target) ||
+                0 !== $menu.has(e.target).length ||
+                $menu.removeClass("show");
+
+        });
+    });
+} else {
+    $(".toc.bg,.toc-toggler").hide();
+}
--- plugins/toc/toc.js
+++ plugins/toc/toc.js
@@ -1,15 +1,80 @@
  /* Table of Content, Credit: blustemy.io/creating-a-table-of-contents-in-javascript */
 
-class TableOfContents { constructor({ from, to }) { this.fromElement = from; this.toElement = to; this.headingElements = this.fromElement.querySelectorAll("h1, h2, h3, h4, h5, h6"); this.tocElement = document.createElement("div"); }; getMostImportantHeadingLevel() { let mostImportantHeadingLevel = 6; for (let i = 0; i < this.headingElements.length; i++) { let headingLevel = TableOfContents.getHeadingLevel(this.headingElements[i]); mostImportantHeadingLevel = (headingLevel < mostImportantHeadingLevel) ? headingLevel : mostImportantHeadingLevel; } return mostImportantHeadingLevel; }; static generateId(headingElement) { return headingElement.textContent.replace(/\s+/g, "_"); }; static getHeadingLevel(headingElement) { switch (headingElement.tagName.toLowerCase()) { case "h1": return 1; case "h2": return 2; case "h3": return 3; case "h4": return 4; case "h5": return 5; case "h6": return 6; default: return 1; } }; generateToc() { let currentLevel = this.getMostImportantHeadingLevel() - 1, currentElement = this.tocElement; for (let i = 0; i < this.headingElements.length; i++) { let headingElement = this.headingElements[i], headingLevel = TableOfContents.getHeadingLevel(headingElement), headingLevelDifference = headingLevel - currentLevel, linkElement = document.createElement("a"); if (!headingElement.id) { headingElement.id = TableOfContents.generateId(headingElement); } linkElement.href = `#${headingElement.id}`; linkElement.textContent = headingElement.textContent; if (headingLevelDifference > 0) { for (let j = 0; j < headingLevelDifference; j++) { let listElement = document.createElement("ol"), listItemElement = document.createElement("li"); listElement.appendChild(listItemElement); currentElement.appendChild(listElement); currentElement = listItemElement; } currentElement.appendChild(linkElement); } else { for (let j = 0; j < -headingLevelDifference; j++) { currentElement = currentElement.parentNode.parentNode; } let listItemElement = document.createElement("li"); listItemElement.appendChild(linkElement); currentElement.parentNode.appendChild(listItemElement); currentElement = listItemElement; } currentLevel = headingLevel; } this.toElement.appendChild(this.tocElement.firstChild); } }
-/* Activation */
-document.addEventListener('DOMContentLoaded', () => new TableOfContents({from: document.querySelector('.post-body'), to: document.querySelector('#tocify')}).generateToc() ); 
+ class TableOfContents {
+     constructor({ from, to }) {
+         this.fromElement = from;
+         this.toElement = to;
+         this.headingElements = this.fromElement.querySelectorAll("h1, h2, h3, h4, h5, h6");
+         this.tocElement = document.createElement("div");
+     };
+     getMostImportantHeadingLevel() {
+         let mostImportantHeadingLevel = 6;
+         for (let i = 0; i < this.headingElements.length; i++) {
+             let headingLevel = TableOfContents.getHeadingLevel(this.headingElements[i]);
+             mostImportantHeadingLevel = (headingLevel < mostImportantHeadingLevel) ? headingLevel : mostImportantHeadingLevel;
+         }
+         return mostImportantHeadingLevel;
+     };
+     static generateId(headingElement) { return headingElement.textContent.replace(/\s+/g, "_"); };
+     static getHeadingLevel(headingElement) {
+         switch (headingElement.tagName.toLowerCase()) {
+             case "h1":
+                 return 1;
+             case "h2":
+                 return 2;
+             case "h3":
+                 return 3;
+             case "h4":
+                 return 4;
+             case "h5":
+                 return 5;
+             case "h6":
+                 return 6;
+             default:
+                 return 1;
+         }
+     };
+     generateToc() {
+         let currentLevel = this.getMostImportantHeadingLevel() - 1,
+             currentElement = this.tocElement;
+         for (let i = 0; i < this.headingElements.length; i++) {
+             let headingElement = this.headingElements[i],
+                 headingLevel = TableOfContents.getHeadingLevel(headingElement),
+                 headingLevelDifference = headingLevel - currentLevel,
+                 linkElement = document.createElement("a");
+             if (!headingElement.id) { headingElement.id = TableOfContents.generateId(headingElement); }
+             linkElement.href = `#${headingElement.id}`;
+             linkElement.textContent = headingElement.textContent;
+             if (headingLevelDifference > 0) {
+                 for (let j = 0; j < headingLevelDifference; j++) {
+                     let listElement = document.createElement("ol"),
+                         listItemElement = document.createElement("li");
+                     listElement.appendChild(listItemElement);
+                     currentElement.appendChild(listElement);
+                     currentElement = listItemElement;
+                 }
+                 currentElement.appendChild(linkElement);
+             } else {
+                 for (let j = 0; j < -headingLevelDifference; j++) { currentElement = currentElement.parentNode.parentNode; }
+                 let listItemElement = document.createElement("li");
+                 listItemElement.appendChild(linkElement);
+                 currentElement.parentNode.appendChild(listItemElement);
+                 currentElement = listItemElement;
+             }
+             currentLevel = headingLevel;
+         }
+         this.toElement.appendChild(this.tocElement.firstChild);
+     }
+ }
+ /* Activation */
+ document.addEventListener('DOMContentLoaded', () => new TableOfContents({ from: document.querySelector('.post-body'), to: document.querySelector('#tocify') }).generateToc());
 
-/* Prepend as ShortCode  */
-$("<div class='tocify-wrap'><div class='tocify-inner'> <a class='tocify-title' href='javascript:;' role='button' title='Table of Content'><span class='tocify-title-text'>Table of Content</span></a> <div id='tocify'/></div></div>").appendTo("#toc");
+ /* Prepend as ShortCode  */
+ $("<div class='tocify-wrap'><div class='tocify-inner'> <a class='tocify-title' href='javascript:;' role='button' title='Table of Content'><span class='tocify-title-text'>Table of Content</span></a> <div id='tocify'/></div></div>").appendTo("#toc");
 
-/* Functionality As Widget */
-$(".tocify-title").each(function(e) {
-	(e = $(this)).on("click", function() {
-		e.toggleClass("is-expanded"), $("#tocify").slideToggle(170);
-	});
-});
+ /* Functionality As Widget */
+ $(".tocify-title").each(function(e) {
+     (e = $(this)).on("click", function() {
+         e.toggleClass("is-expanded"), $("#tocify").slideToggle(170);
+     });
+ });
