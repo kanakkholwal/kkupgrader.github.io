2b6fa8b81c6ce411e4ce54f8e345780d807a288b
Kanak <kkupgrader.fs@gmail.com>
2022-04-28 16:22:19 +0530

    Update main.js

--- genesis-ui/main.js
+++ genesis-ui/main.js
@@ -129,7 +129,7 @@ accordions.forEach((accordion) => {
                         () => {
                             ownBody.classList.remove("active");
                         }, {
-                            once: false,
+                            once: true,
                         }
                     );
                 }
