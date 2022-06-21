15b371c0d04dfbc0f4ffee31c6d7a89fdbb40176
Kanak <kkupgrader.fs@gmail.com>
2022-06-21 21:37:02 +0530

    Update script.js

--- assets/v2/js/script.js
+++ assets/v2/js/script.js
@@ -174,51 +174,13 @@ const ContactForm = document.getElementById("contactForm"),
 const SendContactForm = () => {};
 ContactForm.onsubmit = (e) => {
   e.preventDefault();
-  /* SmtpJS.com - v3.0.0 */
-  var Email = {
-    send: function (a) {
-      return new Promise(function (n, e) {
-        (a.nocache = Math.floor(1e6 * Math.random() + 1)), (a.Action = "Send");
-        var t = JSON.stringify(a);
-        Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) {
-          n(e);
-        });
-      });
-    },
-    ajaxPost: function (e, n, t) {
-      var a = Email.createCORSRequest("POST", e);
-      a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"),
-        (a.onload = function () {
-          var e = a.responseText;
-          null != t && t(e);
-        }),
-        a.send(n);
-    },
-    ajax: function (e, n) {
-      var t = Email.createCORSRequest("GET", e);
-      (t.onload = function () {
-        var e = t.responseText;
-        null != n && n(e);
-      }),
-        t.send();
-    },
-    createCORSRequest: function (e, n) {
-      var t = new XMLHttpRequest();
-      return (
-        "withCredentials" in t
-          ? t.open(e, n, !0)
-          : "undefined" != typeof XDomainRequest
-          ? (t = new XDomainRequest()).open(e, n)
-          : (t = null),
-        t
-      );
-    },
-  };
-  Email.send({
-    SecureToken: SecureToken,
-    To: SendTo,
-    From: email.value,
-    Subject: subject.innerHTML,
-    Body: Body.innerHTML,
-  }).then((message) => alert(message));
+  let myForm = document.getElementById(ContactForm);
+  let formData = new FormData(myForm);
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => console.log("Form successfully submitted"))
+    .catch((error) => alert(error));
 };
