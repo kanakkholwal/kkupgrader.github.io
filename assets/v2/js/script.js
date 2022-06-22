10b246792d96ea1e43bcd7296f5c6fc077f27003
Kanak <kkupgrader.fs@gmail.com>
2022-06-23 00:19:35 +0530

    .

--- assets/v2/js/script.js
+++ assets/v2/js/script.js
@@ -181,6 +181,9 @@ ContactForm.onsubmit = (e) => {
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     body: new URLSearchParams(formData).toString(),
   })
-    .then(() => console.log("Form successfully submitted"))
+    .then(() => {
+      console.log("Form successfully submitted");
+      alert("Form successfully submitted");
+     } )
     .catch((error) => alert(error));
 };
